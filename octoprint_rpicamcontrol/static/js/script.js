078605975d6ce56ae78514e7f42b27ca2c91bed0
Mikk Kiilaspää <mikk36@mikk36.eu>
2015-02-03 20:19:15 +0200

    Disable webcam loading, when control tab is hidden

--- octoprint_rpicamcontrol/static/js/script.js
+++ octoprint_rpicamcontrol/static/js/script.js
@@ -1,13 +1,27 @@
 $(document).ready(function() {
   console.log("RPi Cam Control loaded");
-  function reloadRpiCamControlImage() {
+  var RPiCamControlEnabled = false;
+  function reloadRPiCamControlImage() {
+    if(!RPiCamControlEnabled) return;
     var url = this.src;
+    if(!url) return;
     var urlNew = url.substr(0, url.indexOf("?") + 1) + Date.now();
     this.src = urlNew;
   }
   $("#webcam_image")
-  .on("load", reloadRpiCamControlImage)
+  .on("load", reloadRPiCamControlImage)
   .on("error", function() {
-    setTimeout(reloadRpiCamControlImage, 100);
+    setTimeout(reloadRPiCamControlImage, 100);
+  });
+  $("#tabs a[data-toggle=\"tab\"]").on("show", function(e) {
+    if(e.target.hash === "#control") {
+      RPiCamControlEnabled = true;
+      var image = $("#webcam_image");
+      if(image.attr("src")) {
+        image.attr("src", image.attr("src") + "0");
+      }
+    } else {
+      RPiCamControlEnabled = false;
+    }
   });
 });
